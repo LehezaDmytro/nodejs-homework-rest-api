51eafb9cd4b8b428019bb229d0860d98ac43f1cb
Leheza Dmytro <legeza.dmitriy@icloud.com>
2023-04-14 21:16:07 +0300

    all tasks are completed

--- models/contacts.js
+++ models/contacts.js
@@ -4,6 +4,9 @@ const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "contacts.json");
 
+const updateContacts = async (contacts) =>
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
   return JSON.parse(data);
@@ -22,11 +25,11 @@ const removeContact = async (contactId) => {
     return null;
   }
   const [result] = allContacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  await updateContacts(allContacts);
   return result;
 };
 
-const addContact = async (body) => {
+const addContact = async ({ name, email, phone }) => {
   const newContact = {
     id: nanoid(),
     name,
@@ -35,11 +38,21 @@ const addContact = async (body) => {
   };
   const allContacts = await listContacts();
   allContacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  await updateContacts(allContacts);
   return newContact;
 };
 
-const updateContact = async (contactId, body) => {};
+const updateContact = async (contactId, { name, email, phone }) => {
+  const allContacts = await listContacts();
+
+  const index = allContacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  allContacts[index] = { id: contactId, name, email, phone };
+  await updateContacts(allContacts);
+  return allContacts[index];
+};
 
 module.exports = {
   listContacts,
